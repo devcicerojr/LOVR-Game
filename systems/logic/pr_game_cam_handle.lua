31e0485153c0a6bf7dc6f3c47ff3c465a0d52bb0
devcicerojr <devcicerojr@gmail.com>
2025-07-02 11:03:39 -0300

    added lerp logic to game cam handle

--- systems/logic/pr_game_cam_handle.lua
+++ systems/logic/pr_game_cam_handle.lua
@@ -1,6 +1,7 @@
 local pr_ecs = require'../core/pr_ecs'
 local pr_camera = require'pr_camera'
-
+local pos_smoothing = 100
+local rot_smoothing = 100
 return {
   phase = "logic",
   requires = {"game_cam", "transform"},
@@ -16,7 +17,13 @@ return {
     -- game_cam_rot_offset:mul(entity_rot)
     -- local new_game_cam_pose = lovr.math.newMat4(entity_transform:unpack())
     -- local game_cam_pos_rotated_offset = lovr.math.vec3(game_cam_offset:getPosition()):rotate(entity_transform:getOrientation())
-    pr_camera.game_cam:set(lovr.math.vec3(entity_transform:getPosition()):add(game_cam_pos_offset), lovr.math.vec3(1,1,1), lovr.math.quat(game_cam_rot_offset))
+    local cur_cam_pos = lovr.math.vec3(pr_camera.game_cam:getPosition())
+    local cur_cam_orient = lovr.math.quat(pr_camera.game_cam:getOrientation())
+    local target_cam_pos = lovr.math.vec3(entity_transform:getPosition()):add(game_cam_pos_offset)
+    local target_cam_orient = lovr.math.quat(game_cam_rot_offset)
+    local new_pos = cur_cam_pos:lerp(target_cam_pos, 1 - math.exp(-pos_smoothing * dt))
+    local new_rot = cur_cam_orient:slerp(target_cam_orient, 1, - math.exp(-rot_smoothing * dt))
+    pr_camera.game_cam:set(new_pos, lovr.math.vec3(1,1,1), new_rot)
 
   end
 }
