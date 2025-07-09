049ac49684655f3f716605c8eaaf0bd7380a87c9
devcicerojr <devcicerojr@gmail.com>
2025-07-09 00:22:51 -0300

    minor changes to the outline behavior

--- conf.lua
+++ conf.lua
@@ -17,7 +17,7 @@ function lovr.conf(t)
   t.graphics.debug = false
   t.graphics.vsync = false
   t.graphics.stencil = false
-  t.graphics.antialias = true
+  t.graphics.antialias = false
   t.graphics.shadercache = true
 
   t.modules.headset =  true
